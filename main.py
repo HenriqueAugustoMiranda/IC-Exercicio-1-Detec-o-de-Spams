168e05c9bbb8a9ecc6350257e86cb8ff3e469117
HenriqueAugustoMiranda <rickmteixeira10@gmail.com>
2026-01-06 16:29:14 -0300

    antes de fazer o grid_search para os melhores parâmetros

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from sklearn.svm import LinearSVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, f1_score
 import numpy as np
+from itertools import product
 
 
 # nltk.download("stopwords")
@@ -61,12 +62,14 @@ def pre_processamento(entrada):
     return entrada
 
 
-def tfidf(entrada):
+def tfidf(entrada, min_df = 5, max_df = 1.0, max_features = 5000, ngram_range = (1,1), sublinear_tf = False):
     
     vectorizer = TfidfVectorizer(
-        min_df=5,
-        max_features=5000,
-        ngram_range=(1,1)
+        min_df = min_df,
+        max_df=max_df,
+        max_features = max_features,
+        ngram_range = ngram_range,
+        sublinear_tf=sublinear_tf
     )
 
     tfidf_mat = vectorizer.fit_transform(entrada['text'])
@@ -74,9 +77,14 @@ def tfidf(entrada):
     return tfidf_mat, vectorizer
 
 
-def KNN(X_train, X_test, y_train, y_test):
+def KNN(X_train, X_test, y_train, y_test, n_neighbors = 5, weights='distance'):
 
-    KNC = KNeighborsClassifier(n_neighbors=5)
+    KNC = KNeighborsClassifier(
+        n_neighbors=n_neighbors,
+        weights=weights,
+        metric='cosine',
+        algorithm='brute'
+    )
     KNC.fit(X_train, y_train)
     predict = KNC.predict(X_test)
 
@@ -86,9 +94,14 @@ def KNN(X_train, X_test, y_train, y_test):
     return accuracy, f1
 
 
-def SVM(X_train, X_test, y_train, y_test):
+def SVM(X_train, X_test, y_train, y_test, C=1, loss='squared_hinge', max_iter=5000):
 
-    svm = LinearSVC(random_state=42)
+    svm = LinearSVC(
+        C=C,
+        loss=loss,
+        max_iter=max_iter,
+        random_state=42
+    )
     svm.fit(X_train, y_train)
     predict = svm.predict(X_test)
 
@@ -98,12 +111,12 @@ def SVM(X_train, X_test, y_train, y_test):
     return accuracy, f1
 
 
-def DTC(X_train, X_test, y_train, y_test):
+def DTC(X_train, X_test, y_train, y_test, criterion='gini', max_depth=20, min_samples_split=5,):
     
     dtc = DecisionTreeClassifier(
-        criterion='gini',
-        max_depth=20,
-        min_samples_split=5,
+        criterion=criterion,
+        max_depth=max_depth,
+        min_samples_split=min_samples_split,
         random_state=42
     )
 
@@ -116,13 +129,16 @@ def DTC(X_train, X_test, y_train, y_test):
     return accuracy, f1
 
 
-def classify(X, y):
+def classify(entrada, X, y):
 
+    knn_parameters, svc_parameters, dtc_parameters, tfidf_parameters = grid_search(entrada, X, y)
+
+    X, vectorizer = tfidf(entrada, **tfidf_parameters)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    KNN_accuracy, KNN_f1 = KNN(X_train, X_test, y_train, y_test)
-    SVM_accuracy, SVM_f1 = SVM(X_train, X_test, y_train, y_test)
-    DTC_accuracy, DTC_f1 = DTC(X_train, X_test, y_train, y_test)
+    KNN_accuracy, KNN_f1 = KNN(X_train, X_test, y_train, y_test, **knn_parameters)
+    SVM_accuracy, SVM_f1 = SVM(X_train, X_test, y_train, y_test, **svc_parameters)
+    DTC_accuracy, DTC_f1 = DTC(X_train, X_test, y_train, y_test, **dtc_parameters)
 
     labels = ["KNN", "SVC", "Árvore Binária"]
     accuracy = [KNN_accuracy, SVM_accuracy, DTC_accuracy]
@@ -141,6 +157,98 @@ def classify(X, y):
     mplot.show()
 
 
+def grid_search(entrada, X_base, y):
+    
+    X_train, X_test, y_train, y_test = train_test_split(X_base, y, test_size=0.2, random_state=42)
+
+    tfidf_grid = {
+        'min_df': [1, 2, 3, 5],
+        'max_df': [0.7, 0.8, 0.9, 1.0],
+        'max_features': [2000, 3000, 5000, 8000],
+        'ngram_range': [(1,1), (1,2)],
+        'sublinear_tf': [True, False]
+    }
+    tfidf_parameters = None
+    best_score = -1
+
+    knn_grid = {
+        'n_neighbors' : [3, 5, 7, 9],
+        'weights' : ['uniform', 'distance'],
+    }
+    knn_parameters = None
+    best_knn = -1
+
+    svc_grid = {
+        'C': [0.1, 1, 10],
+        'loss': ['hinge', 'squared_hinge'],
+        'max_iter' : [1000, 3000, 5000]
+    }
+    svc_parameters = None
+    best_svc = -1
+
+    dtc_grid = {
+        'criterion': ['gini', 'entropy', 'log_loss'],
+        'max_depth': [None, 5, 10, 20, 30],
+        'min_samples_split': [2, 5, 10, 20]
+    }
+    dtc_parameters = None
+    best_dtc = -1
+
+    # knn
+    knn_combination = iterar_grid(knn_grid)
+    for knn_params in knn_combination:
+
+        a, f1 = KNN(X_train, X_test, y_train, y_test, **knn_params)
+        if f1 > best_knn:
+            knn_parameters = knn_params
+            best_knn = f1
+
+    # svc
+    svc_combination = iterar_grid(svc_grid)
+    for svc_params in svc_combination:
+
+        a, f1 = SVM(X_train, X_test, y_train, y_test, **svc_params)
+        if f1 > best_svc:
+            svc_parameters = svc_params
+            best_svc = f1
+
+    # dtc
+    dtc_combination = iterar_grid(dtc_grid)
+    for dtc_params in dtc_combination:
+
+        a, f1 = DTC(X_train, X_test, y_train, y_test, **dtc_params)
+        if f1 > best_dtc:
+            dtc_parameters = dtc_params
+            best_dtc = f1
+    
+    # tfidf
+    tfidf_combination = iterar_grid(tfidf_grid)
+    for tfidf_params in tfidf_combination:
+
+        X, vet = tfidf(entrada, **tfidf_params)
+        X_tn, X_ts, y_tn, y_ts = train_test_split(X, y, test_size=0.2, random_state=42)
+
+        k_a, k_f1 = KNN(X_tn, X_ts, y_tn, y_ts, **knn_parameters)
+        s_a, s_f1 = SVM(X_tn, X_ts, y_tn, y_ts, **svc_parameters)
+        d_a, d_f1 = DTC(X_tn, X_ts, y_tn, y_ts, **dtc_parameters)
+
+        tfidf_score = np.mean([k_f1, s_f1, d_f1])
+
+        if tfidf_score > best_score:
+            best_score = tfidf_score
+            tfidf_parameters = tfidf_params
+
+    return knn_parameters, svc_parameters, dtc_parameters, tfidf_parameters
+
+def iterar_grid(grid):
+
+    chaves = list(grid.keys())
+    valores = list(grid.values())
+
+    for combinacao in product(*valores):
+        yield dict(zip(chaves, combinacao))
+
+
 def main():
     
     entrada = pnd.read_csv(ENTRADA_PATH, sep='\t', header=None, names=['label', 'text'])
@@ -152,6 +260,6 @@ def main():
     y = entrada['label'].map({'ham': 0, 'spam': 1})
     print("Shape da matriz TF-IDF:", X.shape)
 
-    classify(X, y)
+    classify(entrada, X, y)
 
 main()
